589c2fc00ca276c87ef03de71df07b7c91391aa1
incheckdev-glitch <incheckdev@gmail.com>
2026-09-22 20:02:21 +0300

    Add lib/company-info.ts

---
+++ lib/company-info.ts
@@ -0,0 +1,9 @@
+export const companyInfo = {
+  name: 'InCheck 360 Holding B.V.',
+  city: 'Enschede',
+  country: 'The Netherlands',
+  email: 'info@incheck360.nl',
+  registrationNumber: null as string | null,
+  vatNumber: null as string | null,
+  streetAddress: null as string | null,
+} as const;
