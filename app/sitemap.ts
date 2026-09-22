3159dbfd518e3ef27fa34bc6e2ba1258eb0ee279
incheckdev-glitch <incheckdev@gmail.com>
2026-09-22 20:54:44 +0300

    Add integrations to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,6 +12,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/industries',
     '/resources',
     '/plans',
+    '/integrations',
     '/about',
     '/contact',
     '/book-demo',
